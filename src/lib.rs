aedbf6f84f7b6de703cc7a89127466fbef05d12e
Yohan Boogaert <yozhgoor@outlook.com>
2021-10-12 23:30:55 +0200

    Build a wasm project (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,101 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use anyhow::{bail, ensure, Context, Result};
+use std::path::Path;
+use std::{fs, process};
+use structopt::StructOpt;
+use wasm_bindgen_cli_support::Bindgen;
+
+#[derive(Debug, StructOpt)]
+pub struct Build {
+    #[structopt(long)]
+    release: bool,
+    #[structopt(short, long)]
+    quiet: bool,
+}
+
+impl Build {
+    pub fn run(
+        &self,
+        crate_name: &'static str,
+        static_dir_path: impl AsRef<Path>,
+        build_dir_path: impl AsRef<Path>,
+    ) -> Result<()> {
+        let metadata = match cargo_metadata::MetadataCommand::new().exec() {
+            Ok(metadata) => metadata,
+            Err(_) => bail!("Cannot get package's metadata"),
+        };
+
+        let mut build_process = process::Command::new("cargo");
+        build_process
+            .current_dir(&metadata.workspace_root)
+            .arg("build");
+
+        if self.release {
+            build_process.arg("--release");
+        }
+
+        if self.quiet {
+            build_process.arg("--quiet");
+        }
+
+        build_process.args([
+            "--target",
+            "wasm32-unknown-unknown",
+            "--package",
+            crate_name,
+        ]);
+
+        ensure!(
+            build_process
+                .status()
+                .context("Could not start cargo")?
+                .success(),
+            "Cargo command failed"
+        );
+
+        if !self.quiet {
+            println!("Generating build...")
+        }
+
+        let input_path = metadata
+            .target_directory
+            .join("wasm32-unknown-unknown")
+            .join("debug")
+            .join(&crate_name.replace("-", "_"))
+            .with_extension("wasm");
+
+        let mut output = Bindgen::new()
+            .input_path(input_path)
+            .out_name("app")
+            .web(true)
+            .expect("web have panic")
+            .debug(!self.release)
+            .generate_output()
+            .context("could not generate WASM bindgen file")?;
+
+        let wasm_js = output.js().to_owned();
+        let wasm_bin = output.wasm_mut().emit_wasm();
+
+        let build_dir_path = build_dir_path.as_ref();
+
+        let wasm_js_path = build_dir_path.join("app.js");
+        let wasm_bin_path = build_dir_path.join("app_bg.wasm");
+
+        if build_dir_path.exists() {
+            fs::remove_dir_all(&build_dir_path)?;
+        }
+
+        let _ = fs::create_dir(&build_dir_path);
+
+        fs::write(wasm_js_path, wasm_js).with_context(|| "Cannot write js file")?;
+        fs::write(wasm_bin_path, wasm_bin).with_context(|| "Cannot write WASM file")?;
+
+        let mut copy_options = fs_extra::dir::CopyOptions::new();
+        copy_options.overwrite = true;
+        copy_options.content_only = true;
+
+        fs_extra::dir::copy(static_dir_path, build_dir_path, &copy_options)
+            .context("Cannot copy static directory")?;
+
+        Ok(())
     }
 }
