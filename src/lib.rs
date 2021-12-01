6d91478d57e1d8f9765d5c63ff674dc48b24d412
Yohan Boogaert <yozhgoor@outlook.com>
2021-12-01 02:11:03 +0100

    Watch a wasm project (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
-use anyhow::{bail, ensure, Context, Result};
-use std::path::Path;
+use std::io::{prelude::*, BufReader};
+use std::net::{IpAddr, SocketAddr, TcpListener, TcpStream};
 use std::{fs, process};
+use std::{path::Path, sync::mpsc};
+
+use anyhow::{bail, ensure, Context, Result};
+use cargo_metadata::camino::Utf8Path;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use structopt::StructOpt;
 use wasm_bindgen_cli_support::Bindgen;
 
@@ -13,16 +18,15 @@ pub struct Build {
 }
 
 impl Build {
-    pub fn run(
+    pub fn execute(
         &self,
         crate_name: &'static str,
         static_dir_path: impl AsRef<Path>,
         build_dir_path: impl AsRef<Path>,
     ) -> Result<()> {
-        let metadata = match cargo_metadata::MetadataCommand::new().exec() {
-            Ok(metadata) => metadata,
-            Err(_) => bail!("Cannot get package's metadata"),
-        };
+        let metadata = cargo_metadata::MetadataCommand::new()
+            .exec()
+            .context("cannot get package's metadata")?;
 
         let mut build_process = process::Command::new("cargo");
         build_process
@@ -47,9 +51,9 @@ impl Build {
         ensure!(
             build_process
                 .status()
-                .context("Could not start cargo")?
+                .context("could not start cargo")?
                 .success(),
-            "Cargo command failed"
+            "cargo command failed"
         );
 
         if !self.quiet {
@@ -86,25 +90,19 @@ impl Build {
 
         let _ = fs::create_dir(&build_dir_path);
 
-        fs::write(wasm_js_path, wasm_js).with_context(|| "Cannot write js file")?;
-        fs::write(wasm_bin_path, wasm_bin).with_context(|| "Cannot write WASM file")?;
+        fs::write(wasm_js_path, wasm_js).with_context(|| "cannot write js file")?;
+        fs::write(wasm_bin_path, wasm_bin).with_context(|| "cannot write WASM file")?;
 
         let mut copy_options = fs_extra::dir::CopyOptions::new();
         copy_options.overwrite = true;
         copy_options.content_only = true;
 
         fs_extra::dir::copy(static_dir_path, build_dir_path, &copy_options)
-            .context("Cannot copy static directory")?;
+            .context("cannot copy static directory")?;
 
         Ok(())
     }
 }
-
-use cargo_metadata::camino::Utf8Path;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::net::{IpAddr, SocketAddr};
-use std::net::{TcpListener, TcpStream};
 
 #[derive(Debug, StructOpt)]
 pub struct DevServer {
@@ -117,14 +115,14 @@ pub struct DevServer {
 impl DevServer {
     pub fn serve(&self, build_dir_path: impl AsRef<Path>) -> Result<()> {
         let address = SocketAddr::new(self.ip, self.port);
-        let listener = TcpListener::bind(&address).context("Cannot bind to the given address")?;
+        let listener = TcpListener::bind(&address).context("cannot bind to the given address")?;
 
         log::info!("Development server at: http://{}", &address);
 
         for mut stream in listener.incoming().filter_map(|x| x.ok()) {
             respond_to_request(&mut stream, &build_dir_path).unwrap_or_else(|e| {
                 let _ = stream.write("HTTP/1.1 400 BAD REQUEST\r\n\r\n".as_bytes());
-                log::warn!("An error occurred: {}", e);
+                log::error!("an error occurred: {}", e);
             });
         }
 
@@ -179,14 +177,93 @@ fn respond_to_request(stream: &mut TcpStream, build_dir_path: impl AsRef<Path>) 
                 )
                 .as_bytes(),
             )
-            .context("Cannot write response")?;
+            .context("cannot write response")?;
 
         std::io::copy(&mut fs::File::open(&full_path)?, stream)?;
     } else {
         stream
             .write("HTTP/1.1 404 NOT FOUND\r\n\r\n".as_bytes())
-            .context("Cannot write response")?;
+            .context("cannot write response")?;
     }
 
     Ok(())
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Watch {}
+
+impl Watch {
+    pub fn execute(
+        &self,
+        build_path: impl AsRef<Path> + std::convert::AsRef<cargo_metadata::camino::Utf8Path>,
+        command: &mut process::Command,
+    ) -> Result<()> {
+        let (tx, rx) = mpsc::channel();
+        let mut watcher: RecommendedWatcher =
+            notify::Watcher::new(tx, std::time::Duration::from_secs(2))
+                .context("could not initialize watcher")?;
+
+        let metadata = cargo_metadata::MetadataCommand::new()
+            .exec()
+            .context("cannot get package's metadata")?;
+        let target_path = &metadata.target_directory;
+        let build_path = &metadata.workspace_root.join(build_path);
+
+        watcher
+            .watch(&metadata.workspace_root, RecursiveMode::Recursive)
+            .context("cannot watch this crate")?;
+
+        let mut child = command.spawn().context("cannot spawn command")?;
+
+        loop {
+            use notify::DebouncedEvent::*;
+
+            let message = rx.recv();
+
+            match &message {
+                Ok(Create(path)) | Ok(Write(path)) | Ok(Remove(path)) | Ok(Rename(_, path))
+                    if !path.starts_with(build_path)
+                        && !path.starts_with(target_path)
+                        && !path
+                            .file_name()
+                            .and_then(|x| x.to_str())
+                            .map(|x| x.starts_with('.'))
+                            .unwrap_or(false) =>
+                {
+                    #[cfg(unix)]
+                    {
+                        use std::convert::TryInto;
+
+                        let now = std::time::Instant::now();
+
+                        unsafe {
+                            libc::kill(
+                                child.id().try_into().expect("cannot get process id"),
+                                libc::SIGTERM,
+                            );
+                        }
+
+                        while now.elapsed().as_secs() < 2 {
+                            std::thread::sleep(std::time::Duration::from_millis(200));
+                            if let Ok(Some(_)) = child.try_wait() {
+                                break;
+                            }
+                        }
+                    }
+
+                    match child.try_wait() {
+                        Ok(Some(_)) => {}
+                        _ => {
+                            let _ = child.kill();
+                            let _ = child.wait();
+                        }
+                    }
+
+                    child = command.spawn().context("cannot spawn command")?;
+                }
+                Ok(_) => {}
+                Err(err) => log::error!("watch error: {}", err),
+            };
+        }
+    }
 }
