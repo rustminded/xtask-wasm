fd74a934281f57d3a57f30e2542eb616d0d0977e
yozhgoor <yozhgoor@outlook.com>
2021-12-09 15:31:58 +0100

    Add logs

--- examples/demo/xtask/src/main.rs
+++ examples/demo/xtask/src/main.rs
@@ -18,30 +18,38 @@ enum Command {
 
 fn main() -> Result<()> {
     env_logger::builder()
-        .filter(Some("xtask_wasm"), log::LevelFilter::Trace)
+        .filter(Some("xtask"), log::LevelFilter::Trace)
         .init();
 
     let mut build_command = process::Command::new("cargo");
     build_command.args(["xtask", "build"]);
 
     let crate_name = "demo-webapp";
-    let static_dir = "demo-webapp/static";
+    let static_dir = "../demo-webapp/static";
     let build_dir = "build";
 
     let opt = Opt::from_args();
 
     match opt.cmd {
         Command::Build(arg) => {
+            log::trace!("Building into {}", build_dir);
             arg.execute(crate_name, static_dir, build_dir)?;
+            log::trace!("Builded");
         }
         Command::StartServer(arg) => {
+            log::trace!("Starting dev server");
             arg.serve(build_dir)?;
+            log::trace!("Shutting down dev server");
         }
         Command::Serve(arg) => {
+            log::trace!("Starting to serve {:?}", build_command);
             arg.watch(build_dir, build_command)?;
+            log::trace!("Serve stopped");
         }
         Command::Watch(arg) => {
+            log::trace!("Starting to watch {:?}", build_command);
             arg.execute(build_dir, build_command)?;
+            log::trace!("Watch stopped");
         }
     }
 
