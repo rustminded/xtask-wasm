ea6e9417bde2ff2424fde85e6592a1294be50e70
yozhgoor <yozhgoor@outlook.com>
2022-01-21 22:45:54 +0100

    After review

--- examples/demo/xtask/src/main.rs
+++ examples/demo/xtask/src/main.rs
@@ -2,8 +2,6 @@ use anyhow::Result;
 use std::process;
 use structopt::StructOpt;
 
-use xtask_wasm::{metadata, wasm_opt};
-
 #[derive(StructOpt)]
 struct Opt {
     #[structopt(long = "log", default_value = "Info")]
@@ -14,10 +12,18 @@ struct Opt {
 
 #[derive(StructOpt)]
 enum Command {
-    Build(xtask_wasm::Build),
-    OptimizedBuild(xtask_wasm::Build),
+    Build(Build),
     Watch(xtask_wasm::Watch),
     Serve(xtask_wasm::DevServer),
+}
+
+#[derive(StructOpt)]
+struct Build {
+    #[structopt(long)]
+    optimize: bool,
+
+    #[structopt(flatten)]
+    base: xtask_wasm::Build,
 }
 
 fn main() -> Result<()> {
@@ -33,24 +39,16 @@ fn main() -> Result<()> {
     match opt.cmd {
         Command::Build(mut arg) => {
             log::info!("Starting to build");
-            arg.static_dir_path("demo-webapp/static");
-            arg.run("demo-webapp")?;
-        }
-        Command::OptimizedBuild(mut arg) => {
-            log::info!("starting an optimized build");
-            arg.static_dir_path("demo-webapp/static");
-            arg.release = true;
-            arg.run("demo-webapp")?;
-            wasm_opt(
-                metadata()
-                    .target_directory
-                    .join("release")
-                    .join("dist")
-                    .join("app_bg.wasm"),
-                0,
-                0,
-                true,
-            )?;
+            arg.base.static_dir_path("demo-webapp/static");
+            let build_dir = arg.base.run("demo-webapp")?;
+            if arg.optimize {
+                xtask_wasm::wasm_opt(
+                    build_dir.join("app_bg.wasm"),
+                    0,
+                    0,
+                    true,
+                )?;
+            }
         }
         Command::Watch(arg) => {
             log::info!("Starting to watch");
--- src/wasm_opt.rs
+++ src/wasm_opt.rs
@@ -6,19 +6,15 @@ use std::{
     process,
 };
 
-fn wasm_opt_url() -> &'static str {
-    lazy_static! {
-        static ref WASM_OPT_URL: String = {
-            format!(
+lazy_static! {
+    static ref WASM_OPT_URL: String = {
+        format!(
                 "https://github.com/WebAssembly/binaryen/releases/download/version_{version}/binaryen-version_{version}-{arch}-{os}.tar.gz",
                 version = "105",
                 arch = platforms::TARGET_ARCH,
                 os = platforms::TARGET_OS,
             )
-        };
-    }
-
-    &WASM_OPT_URL
+    };
 }
 
 pub fn run(
@@ -27,20 +23,21 @@ pub fn run(
     optimization_level: u32,
     debug_info: bool,
 ) -> Result<()> {
+    let input_path = binary_path.as_ref();
+    let output_path = input_path.with_extension("opt");
     let wasm_opt = download_wasm_opt()?;
-    let output_path = binary_path.as_ref().with_extension("opt");
 
     let mut command = process::Command::new(&wasm_opt);
     command
         .stderr(process::Stdio::inherit())
-        .arg(binary_path.as_ref())
-        .args(&[
-            "-o",
-            &output_path.to_str().expect("cannot parse output path"),
-            "-O",
-        ])
-        .args(&["-ol", &optimization_level.to_string()])
-        .args(&["-s", &shrink_level.to_string()]);
+        .arg(input_path)
+        .arg("-o")
+        .arg(&output_path)
+        .arg("-O")
+        .arg("-ol")
+        .arg(optimization_level.to_string())
+        .arg("-s")
+        .arg(shrink_level.to_string());
 
     if debug_info {
         command.arg("-g");
@@ -57,8 +54,8 @@ pub fn run(
         "command `wasm-opt` failed"
     );
 
-    fs::remove_file(binary_path.as_ref())?;
-    fs::rename(&output_path, output_path.with_extension("wasm"))?;
+    fs::remove_file(&input_path)?;
+    fs::rename(&output_path, &input_path)?;
 
     log::info!("WASM optimized");
     Ok(())
@@ -70,7 +67,6 @@ fn download_wasm_opt() -> Result<&'static Path> {
             fn downloaded_binary_path() -> Result<PathBuf> {
                 let cache =
                     binary_install::Cache::at(crate::metadata().target_directory.as_std_path());
-                let url = wasm_opt_url();
 
                 #[cfg(target_os = "macos")]
                 let binaries = &["wasm-opt", "libbinaryen"];
@@ -79,9 +75,9 @@ fn download_wasm_opt() -> Result<&'static Path> {
 
                 log::info!("Downloading wasm-opt");
                 Ok(cache
-                    .download(true, "wasm-opt", binaries, &url)
+                    .download(true, "wasm-opt", binaries, &WASM_OPT_URL)
                     .map_err(|err| err.compat())
-                    .with_context(|| format!("could not download wasm-opt: {}", url))?
+                    .with_context(|| format!("could not download wasm-opt: {}", &WASM_OPT_URL.as_str()))?
                     .expect("install_permitted is always true; qed")
                     .binary("wasm-opt")
                     .map_err(|err| err.compat())?)
